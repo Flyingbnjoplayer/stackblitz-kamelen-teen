43d7546971cdec0f757e3bdda031c315674469f8
Tristan <165851330+Flyingbnjoplayer@users.noreply.github.com>
2026-01-25 21:10:26 +0100

    Uncomment Button export in index.tsx

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -3,7 +3,7 @@
 // Only import components that actually exist in your project
 
 // Common components - uncomment the ones you have:
-// export { Button, buttonVariants } from './button'
+export { Button, buttonVariants } from './button'
 // export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
 // export { Input } from './input'
 // export { Label } from './label'
