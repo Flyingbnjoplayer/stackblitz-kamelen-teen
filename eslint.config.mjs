19ae75a304a002e8ed00bcd6ba3ac28b214f8501
Tristan <165851330+Flyingbnjoplayer@users.noreply.github.com>
2026-02-04 22:03:07 +0000

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,1 +1,11 @@
-export default [{}];
+export default [{
+    import :,nextConfig, from ,'eslint-config-next/core-web-vitals'
+// Your other config imports...
+ 
+:,const :,eslintConfig = [
+  // Your other configurations...
+  ...nextConfig,
+]
+ 
+,export:,: default :eslintConfig
+}];
