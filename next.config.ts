30bab6fdaf9e6265b26fa70fd9da3e9ef09c11f6
Flyingbnjoplayer <trisdejong777@gmail.com>
2026-03-03 20:59:42 +0100

    comi

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-   turbopack: {}, // Add this to silence the error
-  /*eslint: {
-    // Disable ESLint during builds to avoid circular reference error
-    // Type checking is still enforced
-    ignoreDuringBuilds: true,
-  },*/
   images: {
     remotePatterns: [
       {
@@ -14,17 +8,14 @@ const nextConfig: NextConfig = {
         hostname: '**',
       },
     ],
-   },
+  },
   typescript: {
-    // Keep TypeScript checking enabled
     ignoreBuildErrors: false,
-  
   },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
-
 
 export default nextConfig;
