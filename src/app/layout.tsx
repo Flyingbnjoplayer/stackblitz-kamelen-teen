328a662d31bd7f4be0f0448e5ac6a79e9216be97
Flyingbnjoplayer <trisdejong777@gmail.com>
2026-03-03 20:16:08 +0100

    styles fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "@coinbase/onchainkit/styles.css";
 import { ResponseLogger } from "../components/response-logger";
 import { ReadyNotifier } from "../components/ready-notifier";
 import { Providers } from "./providers";
