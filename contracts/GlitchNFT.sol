6a8bdc71d0c0485a8c5623213b6748e178f351dd
Flyingbnjoplayer <trisdejong777@gmail.com>
2026-04-01 23:15:01 +0200

    mkm

--- contracts/GlitchNFT.sol
+++ contracts/GlitchNFT.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 
 /**
  * @title GlitchNFT
