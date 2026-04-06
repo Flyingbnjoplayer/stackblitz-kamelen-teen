f34d80469ee29974ed0e1797a23d33754046522d
Flyingbnjoplayer <trisdejong777@gmail.com>
2026-04-06 16:49:42 +0200

    thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,16 @@ const geistMono = Geist_Mono({
 });
 export const metadata: Metadata = {
   title: "Glitch NFT Studio",
-  description: "Create, mint, and share retro glitch art as NFTs on Base. Real-time effects including RGB split, VHS distortion, and more. Mint your creations and share on Warpcast.",
-  other: { 
+  description: "Create, mint as NFT, and share retro glitch art as NFTs on Base. Real-time effects including RGB split, VHS distortion, and more. Mint your creations and share on farcaster.",
+  openGraph: {
+    images: ['/og-image.png'],
+  },
+  other: {
+     "base:app_id": "69cc1a89f468fca4e20110ba", 
     "fc:frame": JSON.stringify({
       "version": "next",
       "imageUrl": "https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_ae1d2f16-cffb-450f-a2fa-1e4a267b3737-znMJHlnXVSDspmN6lMMSD4iMnQ9wgL",
     }),
-    "base:app_id": "69cc1a89f468fca4e20110ba",
   }
 };
 
